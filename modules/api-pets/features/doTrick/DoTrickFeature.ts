87d06bbe0e702c1d928c823332586d3dd0ffc237
Artemoire <dejan.tot@gmail.com>
2021-11-21 15:17:16 +0100

    fix: phantom } character in pet doing trick

--- modules/api-pets/features/doTrick/DoTrickFeature.ts
+++ modules/api-pets/features/doTrick/DoTrickFeature.ts
@@ -27,7 +27,7 @@ const trickFormatter = (pet: PetDefinition) =>
   pet.race === "muggle" ?
     `${pet.name} the ${pet.type}, did a ${trickAdjective()} ${petTrick()}`
     :
-    `${pet.name} the ${pet.race} ${pet.type}, did a ${trickAdjective()} ${petTrick()}}`;
+    `${pet.name} the ${pet.race} ${pet.type}, did a ${trickAdjective()} ${petTrick()}`;
 
 export class DoTrickFeature {
   constructor(private store: PetStoreDefinition) {
